c39010ea689d3acfc7a2f6fc783e405a244327b5
Ajay Liu <ajayliu2928@gmail.com>
2022-11-18 15:07:00 -0800

    dont use the database_url

--- db.ts
+++ db.ts
@@ -1,17 +1,18 @@
 const Pool = require("pg").Pool;
-require("dotenv").config()
+require("dotenv").config();
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
 
-const proConfig = process.env.DATABASE_URL; //heroku addons
+// const proConfig = process.env.DATABASE_URL; //heroku addons
+const proConfig = devConfig;
 
 const pool = new Pool({
   connectionString:
     process.env.NODE_ENV === "production" ? proConfig : devConfig,
-    ssl:{
-      require: true,
-      rejectUnauthorized:false
-    }
+  ssl: {
+    require: true,
+    rejectUnauthorized: false,
+  },
 });
 
 // module.exports = pool;
